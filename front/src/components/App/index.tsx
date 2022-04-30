bd0416b9477bbf70a352d91cef664e11b8b3071b
BuildTools <unconfigured@null.spigotmc.org>
2022-04-30 17:29:03 +0200

    cors

--- front/src/components/App/index.tsx
+++ front/src/components/App/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Home from '../Pages/Home';
 
 function App() {
   return (
-    <div>Hi Mom!</div>
+    <Home />
   );
 }
 
