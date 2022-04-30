a770fcc1c02921f7db3466f4f4bcbf2419dee5a2
BuildTools <unconfigured@null.spigotmc.org>
2022-04-30 14:57:17 +0200

    Hello world express init

---
+++ api/index.mjs
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT ? process.env.PORT : 80;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
