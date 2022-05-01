94ea04a68fd3400649ab0bc30d43f62e7f1b9601
BuildTools <unconfigured@null.spigotmc.org>
2022-05-01 21:35:00 +0200

    without rabbit

--- api/services/redditQueue.mjs
+++ api/services/redditQueue.mjs
@@ -3,31 +3,31 @@ import firebaseHandler from './FirebaseHandler.mjs';
 
 function connectToRedditDataQueue() {
     try {
-        amqp.connect('amqp://scraper-rabbitmq-service.scraper', function(error0, connection) {
-            if (error0) {
-                throw error0;
-            }
-            connection.createChannel(function(error1, channel) {
-                if (error1) {
-                    throw error1;
-                }
+        // amqp.connect('amqp://scraper-rabbitmq-service.scraper', function(error0, connection) {
+        //     if (error0) {
+        //         throw error0;
+        //     }
+        //     connection.createChannel(function(error1, channel) {
+        //         if (error1) {
+        //             throw error1;
+        //         }
 
-                let queue = 'reddit_data_queue';
+        //         let queue = 'reddit_data_queue';
 
-                channel.assertQueue(queue, {
-                    durable: false
-                });
+        //         channel.assertQueue(queue, {
+        //             durable: false
+        //         });
 
-                console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+        //         console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
 
-                channel.consume(queue, function(msg) {
-                    console.log(" [x] Received %s", JSON.parse(msg.content.toString()));
-                    firebaseHandler.putData(JSON.parse(msg.content.toString()))
-                }, {
-                    noAck: true
-                });
-            });
-        });   
+        //         channel.consume(queue, function(msg) {
+        //             console.log(" [x] Received %s", JSON.parse(msg.content.toString()));
+        //             firebaseHandler.putData(JSON.parse(msg.content.toString()))
+        //         }, {
+        //             noAck: true
+        //         });
+        //     });
+        // });   
     }
     catch(err) {
         alert(err);
