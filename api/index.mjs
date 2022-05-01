aa701d8c65641e265c3e017c4d7116bf1f190f7f
BuildTools <unconfigured@null.spigotmc.org>
2022-05-01 18:18:07 +0200

    for ingress

--- api/index.mjs
+++ api/index.mjs
@@ -11,8 +11,6 @@ app.use(cors());
 
 app.use('/data', dataRouter);
 
-app.get('/', (req, res) => { res.status(200).send("hi!"); });
-
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
 });
