6b977ff05ee5f11f8561ab56331e72e0d7b8dab6
BuildTools <unconfigured@null.spigotmc.org>
2022-05-01 17:51:59 +0200

    wihtout rabbit

--- api/services/redditQueue.mjs
+++ api/services/redditQueue.mjs
@@ -2,31 +2,31 @@ import amqp from 'amqplib/callback_api.js';
 import firebaseHandler from './FirebaseHandler.mjs';
 
 function connectToRedditDataQueue() {
-    amqp.connect('amqp://10.8.15.120', function(error0, connection) {
-        if (error0) {
-            throw error0;
-        }
-        connection.createChannel(function(error1, channel) {
-            if (error1) {
-                throw error1;
-            }
+    // amqp.connect('amqp://10.8.15.120', function(error0, connection) {
+    //     if (error0) {
+    //         throw error0;
+    //     }
+    //     connection.createChannel(function(error1, channel) {
+    //         if (error1) {
+    //             throw error1;
+    //         }
 
-            let queue = 'reddit_data_queue';
+    //         let queue = 'reddit_data_queue';
 
-            channel.assertQueue(queue, {
-                durable: false
-            });
+    //         channel.assertQueue(queue, {
+    //             durable: false
+    //         });
 
-            console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+    //         console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
 
-            channel.consume(queue, function(msg) {
-                console.log(" [x] Received %s", JSON.parse(msg.content.toString()));
-                firebaseHandler.putData(JSON.parse(msg.content.toString()))
-            }, {
-                noAck: true
-            });
-        });
-    });
+    //         channel.consume(queue, function(msg) {
+    //             console.log(" [x] Received %s", JSON.parse(msg.content.toString()));
+    //             firebaseHandler.putData(JSON.parse(msg.content.toString()))
+    //         }, {
+    //             noAck: true
+    //         });
+    //     });
+    // });
 }
 
 export default connectToRedditDataQueue;
