1217efe1ea13340a947ea936ea5f96265dca2b3d
BuildTools <unconfigured@null.spigotmc.org>
2022-05-01 15:12:03 +0200

    pipe for api

--- api/services/redditQueue.mjs
+++ api/services/redditQueue.mjs
@@ -4,31 +4,31 @@ import FirebaseHandler from './FirebaseHandler.mjs';
 const handler = new FirebaseHandler();
 
 function connectToRedditDataQueue() {
-    amqp.connect('amqp://localhost', function(error0, connection) {
-        if (error0) {
-            throw error0;
-        }
-        connection.createChannel(function(error1, channel) {
-            if (error1) {
-                throw error1;
-            }
+    // amqp.connect('amqp://localhost', function(error0, connection) {
+    //     if (error0) {
+    //         throw error0;
+    //     }
+    //     connection.createChannel(function(error1, channel) {
+    //         if (error1) {
+    //             throw error1;
+    //         }
 
-            let queue = 'reddit_data_queue';
+    //         let queue = 'reddit_data_queue';
 
-            channel.assertQueue(queue, {
-                durable: false
-            });
+    //         channel.assertQueue(queue, {
+    //             durable: false
+    //         });
 
-            console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+    //         console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
 
-            channel.consume(queue, function(msg) {
-                console.log(" [x] Received %s", JSON.parse(msg.content.toString()));
-                handler.putData(JSON.parse(msg.content.toString()))
-            }, {
-                noAck: true
-            });
-        });
-    });
+    //         channel.consume(queue, function(msg) {
+    //             console.log(" [x] Received %s", JSON.parse(msg.content.toString()));
+    //             handler.putData(JSON.parse(msg.content.toString()))
+    //         }, {
+    //             noAck: true
+    //         });
+    //     });
+    // });
 }
 
 export default connectToRedditDataQueue;
