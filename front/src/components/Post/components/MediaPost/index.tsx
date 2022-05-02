a29c4ffcb920dba7ebdb94319be67245ca3ae3b7
Marcin Mordecki <marcin.morde@gmail.com>
2022-05-02 23:44:56 +0200

    Added media_carousel handling

--- front/src/components/Post/components/MediaPost/index.tsx
+++ front/src/components/Post/components/MediaPost/index.tsx
@@ -1,12 +1,36 @@
-import post_t from '~/services/post_t';
+import { Carousel } from 'react-bootstrap';
 
-import { media_post_t } from '~/services/post_t';
+import { media_post_t, image_t, gif_t, mp4_t } from '~/services/post_t';
 
 function MediaPost(props: { post: media_post_t }) {
     return (
-        <>
-            media
-        </>
+        <Carousel>
+            {
+                props.post.medias.map((media) => {
+                    if (media.type === "image_t")
+                        return <Carousel.Item>
+                            <img
+                                src= { media.image }
+                                alt="Media Image"
+                            />
+                        </Carousel.Item>
+                    else if (media.type === "gif_t")
+                        return <Carousel.Item>
+                            <img
+                                src= { media.gif }
+                                alt="Media gif"
+                            />
+                        </Carousel.Item>
+                    else if (media.type === "mp4_t")
+                        return <Carousel.Item>
+                            <video controls>
+                                <source src={ media.mp4 } type="video/mp4"></source>
+                                Unable to play media video
+                            </video>
+                        </Carousel.Item>
+                })
+            }
+        </Carousel>
     )
 }
 
