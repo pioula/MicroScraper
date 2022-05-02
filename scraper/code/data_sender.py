84ad72acb97f74fa632d3ba23e6ebb237a70154d
Marcin Mordecki <marcin.morde@gmail.com>
2022-05-02 18:53:58 +0200

    More debug info added

--- scraper/code/data_sender.py
+++ scraper/code/data_sender.py
@@ -7,6 +7,7 @@ RABBIT_DNS = 'scraper-rabbitmq-service.scraper'
 
 # Sends data passed as a dictionary to a given address.
 def send_data(data_dict):
+    print('Setting up conntection with RabbitMQ')
     try:
         connection = pika.BlockingConnection(
             pika.ConnectionParameters(host=RABBIT_DNS))
@@ -14,12 +15,13 @@ def send_data(data_dict):
         sys.stderr.write('Unable to connect to RabbitMQ')
         sys.exit(1)
 
+    print('Connection set up')
     channel = connection.channel()
-
     channel.queue_declare(queue='reddit_data_queue')
+    print('Queue declared')
 
     channel.basic_publish(exchange='', routing_key='reddit_data_queue', body=json.dumps(data_dict))
     connection.close()
 
-    sys.stdout.write('Reddit data sent.')
+    print('Reddit data sent.')
 
