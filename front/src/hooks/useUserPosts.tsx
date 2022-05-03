91dd4a6ba347c5b0036910986b8c35ca346fa455
BuildTools <unconfigured@null.spigotmc.org>
2022-05-03 22:59:51 +0200

    useUserPosts

---
+++ front/src/hooks/useUserPosts.tsx
@@ -0,0 +1,29 @@
+import post_t from "~/services/post_t";
+
+function useUserPosts() {
+    function getUserPosts(userId: string) {
+        return fetch(`http://localhost:80/user?user=${userId}`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        }).then((response) => response.json())
+        .catch((err) => { console.log("error, ", err); return { foo: 'bar' } });
+    }
+
+    function udpateUserPosts(userId: string, posts: Array<post_t>) {
+        return fetch(`http://localhost:80/user?user=${userId}`, {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(posts)
+        });
+    }
+
+    return { getUserPosts, udpateUserPosts };
+}
+
+export default useUserPosts;
