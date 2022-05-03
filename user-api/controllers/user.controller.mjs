61f3bae6af302639a3df7c358b338690c41615d7
BuildTools <unconfigured@null.spigotmc.org>
2022-05-04 00:41:35 +0200

    edit for health checks

--- user-api/controllers/user.controller.mjs
+++ user-api/controllers/user.controller.mjs
@@ -2,9 +2,11 @@ import firebaseHandler from "../services/FirebaseHandler.mjs";
 
 const userController = {
     getUsersPosts: (req, res) => 
-        firebaseHandler.getUsersPosts(req.query.user)
-            .then((posts) => res.status(200).send(posts))
-            .catch((err) => console.log(err)),
+        req.query.user ?
+            firebaseHandler.getUsersPosts(req.query.user)
+                .then((posts) => res.status(200).send(posts))
+                .catch((err) => console.log(err)) :
+            res.status(200).send("OK"),
     updateUsersPosts: (req, res) =>
         firebaseHandler.updateUsersPosts(req.query.user, req.body)
             .then(() => res.status(200).send("OK")),
